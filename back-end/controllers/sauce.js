e41c88b6ae98bc981a602dfd1ab50d450ec8df08
Péault <elise.peault2@gmail.com>
2021-12-10 16:36:58 +0100

    add DELETE (delete a sauce)

--- back-end/controllers/sauce.js
+++ back-end/controllers/sauce.js
@@ -38,18 +38,32 @@ exports.modifySauce = (req, res, next) => {
 
 
 // Delete a sauce object
-// exports.deleteThing = (req, res, next) => {
-//     Sauce.findOne({ _id: req.params.id })
-//     .then(sauce => {
-//         const filename = sauce.imageUrl.split('/images/')[1];
-//         fs.unlink(`images/${filename}`, () => {
-//             Sauce.deleteOne({ _id: req.params.id }) 
-//             .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-//             .catch(error => res.status(400).json({ error })); 
-//         });
-//     })
-//     .catch (error => res.status(500).json({ error }));
-// };
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({ _id: req.params.id }) 
+            .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+            .catch(error => res.status(400).json({ error })); 
+        });
+    })
+    .catch (error => res.status(500).json({ error }));
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id : req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split("/images/")[1]
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({_id : req.params.id})
+    .then(res.status(200).json({ message: "Sauce supprimée" }))
+    .catch(error => res.status(400).json({ error }))
+    
+      })
+    })
+    .catch(error => res.status(500).json({ error }))
+  }
 
 
 // Export the function that displays all sauces
