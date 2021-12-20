bfe4af87dfe8c594c3114ff35fae94454774debd
Péault <elise.peault2@gmail.com>
2021-12-20 17:28:50 +0100

    add rate limit login/signup

---
+++ back-end/middleware/rate-limit.js
@@ -0,0 +1,10 @@
+// IMPORT rate limit package (      )
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes 
+    max: 3, // Start blocking after 3 requests
+    message: "Too many failed connection attempts. Please try again in 15 minutes",
+});
+
+module.exports = { limiter };
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -5,9 +5,12 @@ const router = express.Router();
 // Import "userCtrl (to link the functions with the different routes)
 const userCtrl = require('../controllers/user');
 
+// IMPORT middlewares 
+const rateLimit = require('../middleware/rate-limit');
+
 // CREATION OF ROUTES
-router.post('/signup', userCtrl.signup);  
-router.post('/login', userCtrl.login);
+router.post('/signup', rateLimit.limiter, userCtrl.signup);  
+router.post('/login', rateLimit.limiter, userCtrl.login);
 
 // Export the router (So it can be imported in app.js)
 module.exports = router;
