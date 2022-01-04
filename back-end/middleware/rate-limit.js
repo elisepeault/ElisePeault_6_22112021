237a18f3d82c2929152b13a554d0213b2a904418
Péault <elise.peault2@gmail.com>
2022-01-04 16:38:38 +0100

    rate limite & gitignore maj

--- back-end/middleware/rate-limit.js
+++ back-end/middleware/rate-limit.js
@@ -3,8 +3,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes 
-    max: 5, // Start blocking after 3 requests
-    message: "Too many failed connection attempts. Please try again in 15 minutes",
+    max: 10, // Start blocking after 3 requests
 });
 
 module.exports = { limiter };
