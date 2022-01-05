b9fc8ee61190eb55deff1a91174a17dc03e5efbb
Péault <elise.peault2@gmail.com>
2022-01-05 12:42:47 +0100

    Maj Read me

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,11 +1,13 @@
 // IMPORT jsonwebtoken package
 const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+
 // Middleware which will protect the selected routes and verify that the user is authenticated before authorizing the sending of his requests.
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
